f61f70b34abd64815402c75562481a5be18612ab
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-21 13:50:46 +0000

    Add header file

---
+++ include/fractol.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/21 13:50:24 by agiedroi          #+#    #+#             */
+/*   Updated: 2025/09/21 13:50:25 by agiedroi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <math.h>
+# include "libft.h"
+# include "mlx.h"
+#endif
