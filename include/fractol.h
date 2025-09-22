3e1dcb550cd19238333a4b1b15877482329ab487
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-22 13:21:37 +0000

    Add error message macro

--- include/fractol.h
+++ include/fractol.h
@@ -6,7 +6,7 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/21 13:50:24 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/21 13:50:25 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/22 13:15:44 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,7 @@
 # include <math.h>
 # include "libft.h"
 # include "mlx.h"
+
+# define ERROR_MESSAGE "Usage: \"./fractol mandelbrot\" or \
+	\"./fractol julia <Re> <Im>\"\n"
 #endif
