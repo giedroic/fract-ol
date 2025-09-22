ecee373efdde749046e213d68eb79d5b145422ea
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-22 14:40:25 +0000

    Create an image data

--- src/fractal_init.c
+++ src/fractal_init.c
@@ -6,23 +6,43 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 13:54:48 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/22 14:11:09 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/22 14:39:28 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractal.h"
+
+static void malloc_error(void);
 
 void	fractal_init(t_fractal *fractal)
 {
 	fractal->mlx = mlx_init();
 	if (fractal->mlx == NULL)
 	{
-		;
+		malloc_error();
 	}
-	fractal->mlx_win = mlx_new_windows(fractal->mlx, WIDTH, HEIGHT, fractal->name);
+	fractal->mlx_win = mlx_new_windows(fractal->mlx, WIDTH, HEIGHT,
+			fractal->name);
 	if (fractal->mlx_win == NULL)
 	{
 		mlx_destroy_display(fractal->mlx);
 		free(fractal->mlx);
+		malloc_error();
 	}
+	fractal->img.img = mlx_new_image(fractal->mlx, WIDTH, HEIGHT);
+	if (NULL == fractal->img.img)
+	{
+		mlx_destroy_window(fractal->mlx, fractal->mlx_win);
+		mlx_destroy_display(fractal->mlx);
+		free(fractal->mlx);
+		malloc_error();
+	}
+	fractal->img.addr = mlx_get_data_addr(fractal->img.img, &fractal->img.bpp,
+			&fractal->img.size_len, &fractal->endian);
+}
+
+static void malloc_error(void)
+{
+	perror("malloc error");
+	exit(EXIT_FAILURE);
 }
