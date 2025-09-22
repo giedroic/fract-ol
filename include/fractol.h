95256027d0345e11a09640d4592c7a426bb10a05
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-22 14:03:23 +0000

    Add name variable

--- include/fractol.h
+++ include/fractol.h
@@ -6,7 +6,7 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/21 13:50:24 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/22 13:49:22 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/22 14:02:29 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,9 @@
 # define ERROR_MESSAGE "Usage: \"./fractol mandelbrot\" or \
 \"./fractol julia <Re> <Im>\"\n"
 
+# define WIDTH 1000
+# define HEIGHT 1000
+
 typedef struct s_img
 {
 	void	*img;
@@ -32,6 +35,7 @@ typedef struct s_img
 
 typedef struct s_fractal
 {
+	char	*name;
 	void	*mlx;
 	void	*mlx_win;
 	t_img	img;
