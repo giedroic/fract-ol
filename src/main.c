1db1c057725af001e08f5490ae614a4039ec9afc
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-22 13:30:15 +0000

    Fix syntax

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/21 13:28:04 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/21 15:08:20 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/22 13:29:56 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 int	main(int argc, char *argv[])
 {
-	if (argc == 2 && !ft_strncmp(argv[1], "mandelbrot", 10))
+	if ((argc == 2 && !ft_strncmp(argv[1], "mandelbrot", 10))
+		|| (argc == 4 && !ft_strncmp(argv[1], "julia", 5)))
 	{
-
 	}
 	else
 	{
 		ft_putstr_fd(ERROR_MESSAGE, STDERR_FILENO);
+		exit(EXIT_FAILURE);
 	}
 	return (0);
 }
