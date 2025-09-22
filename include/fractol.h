4201970245cdae0335a12c2470979450cce3e73b
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-22 15:13:39 +0000

    Change name of the struct

--- include/fractol.h
+++ include/fractol.h
@@ -6,12 +6,13 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/21 13:50:24 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/22 14:42:02 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/22 15:13:19 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
+# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <math.h>
@@ -20,25 +21,24 @@
 
 # define ERROR_MESSAGE "Usage: \"./fractol mandelbrot\" or \
 \"./fractol julia <Re> <Im>\"\n"
-
 # define WIDTH 1000
 # define HEIGHT 1000
 
-typedef struct s_img
+typedef struct s_data
 {
 	void	*img;
 	char	*addr;
 	int		bpp;
 	int		line_len;
 	int		endian;
-}	t_img;
+}	t_data;
 
 typedef struct s_fractal
 {
 	char	*name;
 	void	*mlx;
 	void	*mlx_win;
-	t_img	img;
+	t_data	img;
 }	t_fractal;
 
 void	fractal_init(t_fractal *fractal);
