38ba350fe6c3d1349d4a0e4fc0828b04fd64faf7
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-22 13:28:56 +0000

    Remove tab from an error message

--- include/fractol.h
+++ include/fractol.h
@@ -6,7 +6,7 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/21 13:50:24 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/22 13:15:44 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/22 13:28:38 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@
 # include "mlx.h"
 
 # define ERROR_MESSAGE "Usage: \"./fractol mandelbrot\" or \
-	\"./fractol julia <Re> <Im>\"\n"
+\"./fractol julia <Re> <Im>\"\n"
 #endif
