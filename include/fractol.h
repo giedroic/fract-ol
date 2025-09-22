51e91b830125691584051a3b0419e646343ea33a
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-22 13:49:42 +0000

    Add initial fractal structure

--- include/fractol.h
+++ include/fractol.h
@@ -6,7 +6,7 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/21 13:50:24 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/22 13:28:38 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/22 13:49:22 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,20 @@
 
 # define ERROR_MESSAGE "Usage: \"./fractol mandelbrot\" or \
 \"./fractol julia <Re> <Im>\"\n"
+
+typedef struct s_img
+{
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		line_len;
+	int		endian;
+}	t_img;
+
+typedef struct s_fractal
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_img	img;
+}	t_fractal;
 #endif
