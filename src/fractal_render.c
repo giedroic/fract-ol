9132e5ce52249f4ffcc1c251c1843f0f675102f2
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-23 19:40:40 +0000

    Implement mlx_pixel_put function

--- src/fractal_render.c
+++ src/fractal_render.c
@@ -6,13 +6,14 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 15:19:32 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/23 18:59:44 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/23 19:40:22 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
 static void	handle_pixel(int x, int y, t_fractal *fractal);
+static void	ft_mlx_pixel_put(t_data *data, int x, int y, int color);
 
 void	fractal_render(t_fractal *fractal)
 {
@@ -34,9 +35,9 @@ static void	handle_pixel(int x, int y, t_fractal *fractal)
 {
 	t_complex	z;
 	t_complex	c;
-	double	tmp_re;
-	int	color;
-	int	i;
+	double		tmp_re;
+	int			color;
+	int			i;
 
 	z.re = 0.0;
 	z.im = 0.0;
@@ -53,7 +54,16 @@ static void	handle_pixel(int x, int y, t_fractal *fractal)
 		++i;
 		if (sqrt(pow(z.re, 2) + pow(z.im, 2)) > fractol->escape_radius)
 		{
+			color = map(i, BLACK, WHITE, fractal->iterations);
 			return ;
 		}
 	}
+}
+
+static void	ft_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bpp / 8));
+	*(unsigned int *) dst = color;
 }
