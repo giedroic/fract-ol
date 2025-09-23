d11a282ff188dbc1e5c002f50f3a7b544386489d
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-23 19:56:25 +0000

    Add two operation functions to shorten the syntax

--- src/fractal_render.c
+++ src/fractal_render.c
@@ -6,14 +6,16 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 15:19:32 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/23 19:41:06 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/23 19:54:30 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-static void	handle_pixel(int x, int y, t_fractal *fractal);
-static void	ft_mlx_pixel_put(t_data *data, int x, int y, int color);
+static void			handle_pixel(int x, int y, t_fractal *fractal);
+static void			ft_mlx_pixel_put(t_data *data, int x, int y, int color);
+static t_complex	square_complex(t_complex z);
+static t_complex	sum_complex(t_complex z, t_complex c);
 
 void	fractal_render(t_fractal *fractal)
 {
@@ -46,19 +48,16 @@ static void	handle_pixel(int x, int y, t_fractal *fractal)
 	i = 0;
 	while (i < fractal->iterations)
 	{
-		tmp_re = pow(z.re, 2) - pow(z.im, 2);
-		z.im = 2 * z.re * z.im;
-		z.re = tmp_re;
-		z.re += c.re;
-		z.im += c.im;
-		++i;
+		z = sum_complex(square_complex(z), c);
 		if (sqrt(pow(z.re, 2) + pow(z.im, 2)) > fractol->escape_radius)
 		{
 			color = map(i, BLACK, WHITE, fractal->iterations);
 			ft_mlx_pixel_put(fractal->img, x, y, color);
 			return ;
 		}
+		++i;
 	}
+	ft_mlx_pixel_put(fractal->img, x, y, ACID_GREEN);
 }
 
 static void	ft_mlx_pixel_put(t_data *data, int x, int y, int color)
@@ -67,4 +66,21 @@ static void	ft_mlx_pixel_put(t_data *data, int x, int y, int color)
 
 	dst = data->addr + (y * data->line_length + x * (data->bpp / 8));
 	*(unsigned int *) dst = color;
+}
+
+static t_complex	square_complex(t_complex z)
+{
+	int	tmp_re;
+
+	tmp_re = pow(z.re, 2) - pow(z.im, 2);
+	z.im = 2 * z.re * z.im;
+	z.re = tmp_re;
+	return (z);
+}
+
+static t_complex	sum_complex(t_complex z, t_complex c)
+{
+	z.re += c.re;
+	z.im += c.im;
+	return (z);
 }
