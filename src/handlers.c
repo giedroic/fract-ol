d3285bfe268cbfcbe0755dc41ab214ffb719d690
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-24 17:03:00 +0000

    Add key conditions to move around the fractal

--- src/handlers.c
+++ src/handlers.c
@@ -6,7 +6,7 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 16:16:34 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/24 16:29:23 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/24 16:54:04 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,15 @@
 int	key_handler(int keysym, t_fractal *fractal)
 {
 	if (keysym == XK_Escape)
-	{
-		exit(EXIT_SUCCESS);		
-	}
-	(void) fractal;
-	return (0);
+		;
+	else if (keysym == XK_Left)
+		fractal->re_shift += 0.1;
+	else if (keysym == XK_Right)
+		fractal->re_shift -= 0.1;
+	else if (keysym == XK_Up)
+		fractal->im_shift -= 0.1;
+	else if (keysym == XK_Down)
+		fractal->im_shift += 0.1;
+	fractal_render(fractal);
+	return (EXIT_SUCCESS);
 }
