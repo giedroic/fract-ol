08e5d1081e8d8a8a387d3e8280f06510a130ee41
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-24 17:22:33 +0000

    Change return type for close handler

--- src/handlers.c
+++ src/handlers.c
@@ -6,13 +6,13 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 16:16:34 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/24 17:20:23 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/24 17:21:58 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-void	close_handler(t_fractal *fractal)
+int	close_handler(t_fractal *fractal)
 {
 	mlx_destroy_image(fractal->mlx, fractal->img.img);
 	mlx_destroy_window(fractal->mlx, fractal->mlx_win);
