dab10bea4c85420cccbe776a6eaa2ced887109a6
Aleksander Giedroic <agiedroi@c3r12s2.42warsaw.pl>
2025-10-31 18:37:22 +0100

    Make use of c variable

--- src/fractal_render.c
+++ src/fractal_render.c
@@ -6,7 +6,7 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 15:19:32 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/10/30 20:33:15 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/10/31 18:37:01 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,8 +44,10 @@ static void	handle_pixel(int x, int y, t_fractal *fractal)
 	int			color;
 	int			i;
 
-	z.re = map(x, -2, 2, WIDTH);
-	z.im = map(y, 2, -2, HEIGHT);
+	z.re = 0.0.;
+	z.im = 0.0;
+	c.re = map(x, -2, 2, WIDTH);
+	c.im = map(y, 2, -2, HEIGHT);
 	i = 0;
 	while (i < fractal->iterations)
 	{
