7ef836f7823dfa573a4f703881120c536b2c3500
Aleksander Giedroic <agiedroi@c3r12s2.42warsaw.pl>
2025-10-31 18:38:54 +0100

    Remove a dot from a varialbe's value

--- src/fractal_render.c
+++ src/fractal_render.c
@@ -6,7 +6,7 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 15:19:32 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/10/31 18:37:01 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/10/31 18:38:30 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ static void	handle_pixel(int x, int y, t_fractal *fractal)
 	int			color;
 	int			i;
 
-	z.re = 0.0.;
+	z.re = 0.0;
 	z.im = 0.0;
 	c.re = map(x, -2, 2, WIDTH);
 	c.im = map(y, 2, -2, HEIGHT);
